243d17025bf3b3576ac5cb3b4be3ca33e9c5745b
Britney <britney.gibbs@student.montana.edu>
2019-05-13 16:03:05 -0600

    Initial Django startproject

--- cbeImageDB/images/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-from .models import Image
-
-
-class UploadFileForm(forms.ModelForm):
-
-    class Meta:
-        model = Image
-        fields = ['document', 'lab', ]
--- cbeImageDB/images/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-
-app_name = 'images'
-urlpatterns = [
-    path('upload-image/', views.upload_file, name='upload'),
-    ]
