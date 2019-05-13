db9a003a4356a5f1fd295094a73d8ed6c076be2e
Britney <britney.gibbs@student.montana.edu>
2019-05-13 16:12:20 -0600

    Added image model

--- cbeImageDB/images/models.py
+++ cbeImageDB/images/models.py
@@ -6,3 +6,17 @@ class Lab(models.Model):
 
     def __str__(self):
             return self.name
+
+
+class Image(models.Model):
+    lab = models.ForeignKey(Lab, on_delete=models.CASCADE)
+    brief_description = models.CharField(max_length=1000)
+    date = models.DateField(("Date"), auto_now_add=True)
+    document = models.FileField()
+    path = models.CharField(max_length=500)
+    image_name = models.CharField(max_length=100)
+
+
+@receiver(models.signals.post_delete, sender=Image)
+def post_delete_file(sender, instance, *args, **kwargs):
+    instance.document.delete(save=False)
