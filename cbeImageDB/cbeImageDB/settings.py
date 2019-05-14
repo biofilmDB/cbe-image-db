daf174f9226a82e68adc765cdb24a9bed56e03be
Britney <britney.gibbs@student.montana.edu>
2019-05-13 21:18:36 -0600

    Added file directory

--- cbeImageDB/cbeImageDB/settings.py
+++ cbeImageDB/cbeImageDB/settings.py
@@ -119,3 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Path for storing files
+MEDIA_ROOT = 'files'  #config('MEDIA_ROOT')
+MEDIA_URL = '/files/'
