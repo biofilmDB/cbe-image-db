906b4fcc83a5427064076c82b96982fbfece484d
Britney <britney.gibbs@student.montana.edu>
2019-05-30 16:24:16 -0600

    View list of images

--- cbeImageDB/images/urls.py
+++ cbeImageDB/images/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('upload-image/', views.upload_file, name='upload'),
     path('<int:pk>/success/', views.ImageDetailsView.as_view(),
          name='image_details'),
-    ]
+    path('view_by_lab/', views.ImageThumbnailsView.as_view(),
+         name='view_by_lab'),
+]
