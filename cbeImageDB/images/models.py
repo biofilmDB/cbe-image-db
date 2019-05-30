c0ddb961d397f13650bd8a1d63a61619c21dfb52
Britney <britney.gibbs@student.montana.edu>
2019-05-30 14:01:55 -0600

    Add to lab name directory

--- cbeImageDB/images/models.py
+++ cbeImageDB/images/models.py
@@ -9,11 +9,17 @@ class Lab(models.Model):
             return self.name
 
 
+def lab_directory_path(instance, filename):
+    # file will be uploaded to MEDIA_ROOT/user_<id>/<filename>
+    formated_lab = str(instance.lab).replace(' ', '_')
+    return '{0}/{1}'.format(formated_lab, filename)
+
+
 class Image(models.Model):
     lab = models.ForeignKey(Lab, on_delete=models.PROTECT)
     brief_description = models.CharField(max_length=1000)
     date = models.DateField(("Date"), auto_now_add=True)
-    document = models.ImageField(upload_to=str(lab.name).replace(' ', '_'))
+    document = models.ImageField(upload_to=lab_directory_path)
     path = models.CharField(max_length=500)
     image_name = models.CharField(max_length=100)
 
