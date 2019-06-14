fa5782364d013d4a4a10b25f52af2aac934a1db2
Britney <britney.gibbs@student.montana.edu>
2019-06-14 13:01:14 -0600

    Fixed filtering name

--- cbeImageDB/images/views.py
+++ cbeImageDB/images/views.py
@@ -11,8 +11,9 @@ class ImagerAutocomplete(autocomplete.Select2QuerySetView):
 
     def get_queryset(self):
         qs = Imager.objects.all()
+        # import ipdb; ipdb.set_trace()
         if self.q:
-            qs = qs.filter(name__istartswith=self.q)
+            qs = qs.filter(imager_name__istartswith=self.q)
         return qs
 
 
