b232fa6d939364cbf8b9dfba5f1451337437837d
Britney <britney.gibbs@student.montana.edu>
2019-07-09 10:34:56 -0600

    Get a list of all objectives used

---
+++ cbeImageDB/images/search_utils.py
@@ -0,0 +1,17 @@
+from .models import Microscope_settings
+
+
+def get_objectives():
+    query = Microscope_settings.objects.all()
+    objectives = []
+    for ms in query:
+        objectives.append(ms.objective)
+
+    objectives = list(set(objectives))
+    obj_string = []
+    for o in objectives:
+        if o.is_integer():
+            obj_string.append(str(int(o)) + 'x')
+        else:
+            obj_string.append(str(o) + 'x')
+    return obj_string
