5e071e123297701f1cbdc84dd2951feb5f2a918b
Britney <britney.gibbs@student.montana.edu>
2019-08-08 11:56:51 -0600

    Made file to make index from tutorial

---
+++ cbeImageDB/images/documents.py
@@ -0,0 +1,30 @@
+from django_elasticsearch_dsl import Document
+from django_elasticsearch_dsl.registries import registry
+from .models import Image
+
+
+@registry.register_document
+class ImageDocument(Document):
+    class Index:
+        # Name of the Elasticsearch index
+        name = 'images'
+        # See Elasticsearch Indices API reference for available settings
+        settings = {'number_of_shards': 1,
+                    'number_of_replicas': 0}
+
+    class Django:
+        model = Image # The model associated with this Document
+
+        # The fields of the model you want to be indexed in Elasticsearch
+        fields = [
+            'brief_description',
+        ]
+
+        # Ignore auto updating of Elasticsearch when a model is saved
+        # or deleted:
+        # ignore_signals = True
+        # Don't perform an index refresh after every update (overrides global setting):
+        # auto_refresh = False
+        # Paginate the django queryset used to populate the index with the specified size
+        # (by default there is no pagination)
+        # queryset_pagination = 5000
