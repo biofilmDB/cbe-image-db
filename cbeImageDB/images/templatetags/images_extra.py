eadcb3d476bae2b4adafea6f33a4210a90db45a4
Britney <britney.gibbs@student.montana.edu>
2019-08-15 11:32:59 -0600

    Changed organism list to experiment

--- cbeImageDB/images/templatetags/images_extra.py
+++ cbeImageDB/images/templatetags/images_extra.py
@@ -26,9 +26,7 @@ def get_lab_list(context, **kwargs):
     labs = [str(x) for x in context['experiment'].lab.all()]
     return ', '.join(labs)
 
-"""
 @register.simple_tag(takes_context=True)
 def get_organism_list(context, **kwargs):
-    organ = [str(x) for x in context['image'].organism.all()]
+    organ = [str(x) for x in context['experiment'].organism.all()]
     return ', '.join(organ)
-"""
