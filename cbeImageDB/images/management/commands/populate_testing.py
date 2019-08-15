69506838b516ac55d6617be121fcc5f40b56b40f
Britney <britney.gibbs@student.montana.edu>
2019-08-15 10:22:19 -0600

    Refactored model names

--- cbeImageDB/images/management/commands/populate_testing.py
+++ cbeImageDB/images/management/commands/populate_testing.py
@@ -1,31 +1,30 @@
 from django.core.management.base import BaseCommand
-from images.models import Lab, Imager, Microscope, Microscope_settings, Medium
-from images.models import Organism
+from . import models as m
 
 
 class Command(BaseCommand):
     help = 'Creates two labs, test lab 1 and 2 and an imager Jane Doe'
 
     def _create_labs(self):
-        tl1 = Lab(pi_name='test lab 1')
+        tl1 = m.Lab(pi_name='test lab 1')
         tl1.save()
 
-        tl2 = Lab(pi_name='test lab 2')
+        tl2 = m.Lab(pi_name='test lab 2')
         tl2.save()
 
     def _create_imager(self):
-        Imager(imager_name='Jane Doe').save()
+        m.Imager(imager_name='Jane Doe').save()
 
     def _create_microscope(self):
-        med = Medium(medium_type="Plasma")
+        med = m.Medium(medium_type="Plasma")
         med.save()
-        mic = Microscope(microscope_name='Test Scope')
+        mic = m.Microscope(microscope_name='Test Scope')
         mic.save()
-        Microscope_settings(microscope=mic, objective=63, medium=med).save()
+        m.Microscope_settings(microscope=mic, objective=63, medium=med).save()
 
     def _create_organism(self):
-        Organism.objects.create(organism_name='Test organism', storid='123',
-                                ncbi_id='ncbi25')
+        m.Organism.objects.create(organism_name='Test organism', storid='123',
+                                  ncbi_id='ncbi25')
 
     def handle(self, *args, **options):
         self._create_labs()
