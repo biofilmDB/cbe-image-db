a4e8f9a64f030223f5011bf14c58a7b653570d59
Britney <britney.gibbs@student.montana.edu>
2019-08-16 13:28:19 -0600

    Added about site url

--- cbeImageDB/images/urls.py
+++ cbeImageDB/images/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 app_name = 'images'
 urlpatterns = [
+    path('about/', views.AboutSite.as_view(), name='about'),
     path('upload-image/', views.UploadImageView.as_view(), name='upload'),
     path('upload-success/<int:pk>', views.ImageUploadSuccessView.as_view(),
          name='upload_success'),
