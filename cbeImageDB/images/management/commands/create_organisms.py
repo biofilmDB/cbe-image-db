86d0160294d78531bd3e1a6976c33544290e20ae
Britney <britney.gibbs@student.montana.edu>
2019-08-21 10:51:33 -0600

    Switched to external file for adding database objects

--- cbeImageDB/images/management/commands/create_organisms.py
+++ cbeImageDB/images/management/commands/create_organisms.py
@@ -1,22 +1,10 @@
 from django.core.management.base import BaseCommand
-from images.models import Organism
-import pandas
+from .import management_commands as mm
 
 
 class Command(BaseCommand):
-    help = "Reads in a list of organisms from a file named organisms.csv and populates the database"
-
-    def _generate_organisms(self):
-        csv = pandas.read_csv('organisms/organisms.csv', sep='|')
-
-        print('Total number of organisms to add: {}'.format(len(csv)))
-        for index, row in csv.iterrows():
-            if index % 5000 == 0 and index != 0:
-                print('Added organism number: {}'.format(index))
-
-            Organism.objects.create(storid=row[0], ncbi_id=row[1],
-                                    organism_name=row[2])
-
+    help = "Reads in a list of organisms from a file named organisms.csv and \
+            populates the database"
 
     def handle(self, *args, **options):
-        self._generate_organisms()
+        mm.create_organisms()
