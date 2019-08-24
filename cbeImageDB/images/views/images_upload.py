a169fc7c164dcb1878cf0ab4c79236808819c2f1
Britney <britney.gibbs@student.montana.edu>
2019-08-24 16:20:12 -0600

    Switched to listview, added pagination

--- cbeImageDB/images/views/images_upload.py
+++ cbeImageDB/images/views/images_upload.py
@@ -5,6 +5,7 @@ from django.urls import reverse
 from template_names import TemplateNames
 from images import search_utils as su
 from multi_form_view import MultiFormView
+from datetime import datetime
 
 
 class UploadImageView(TemplateNames, MultiFormView):
@@ -26,26 +27,39 @@ class UploadImageView(TemplateNames, MultiFormView):
                                content=image.document)
         image.save()
         return HttpResponseRedirect(reverse('images:upload_success',
-                                            args=(experiment.id,)))
+                                            {'experiment': experiment.id}))
 
 
-class ImageUploadSuccessView(TemplateNames, genViews.DetailView):
+class ImageUploadSuccessView(TemplateNames, genViews.ListView):
     """ Shows the details of an image. It is where a sucessfull image upload is
     redirected to."""
-    model = models.Experiment
+    model = models.Image
+    context_object_name = 'image_obj_list'
+    paginate_by = 15
+
+    def get_queryset(self):
+        e = models.Experiment.objects.get(id=self.kwargs['experiment'])
+        qs = e.image_set.all()
+
+        if not self.request.user.is_superuser:
+            qs = qs.filter(release_date__lte=datetime.now())
+
+        return qs
+
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        # get the image
-        e = kwargs['object']
-        images = e.image_set.order_by('-pk')
+        e = models.Experiment.objects.get(id=self.kwargs['experiment'])
+        context['experiment'] = str(e)
+        context['experiment_pk'] = e.pk
         context['get_experiment_details'] = su.get_html_experiment_list(e)
         context['get_image_details'] = []
         # Make dictionarys for each image
-        for image in images:
+        for image in context['image_obj_list']:
             t = su.get_html_image_dict(image, ['microscope setting', 'imager',
                                                'date taken', 'date uploaded'])
             context['get_image_details'].append(t)
+
         return context
 
 
