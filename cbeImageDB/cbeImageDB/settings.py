212d9df08ff50e1fc33c6c1d9dab2dc7b8f2ecd5
Britney <britney.gibbs@student.montana.edu>
2019-09-09 13:22:55 -0600

    Migrated db to postgres

--- cbeImageDB/cbeImageDB/settings.py
+++ cbeImageDB/cbeImageDB/settings.py
@@ -96,8 +96,12 @@ WSGI_APPLICATION = 'cbeImageDB.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASS'),
+        'HOST': 'localhost',
+        'PORT': '5433',
     }
 }
 
