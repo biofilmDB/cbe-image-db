fc91aa66906c9e08be44db1854fd8fa457464234
EarthSquirrel <britneyandcaddy1@gmail.com>
2019-12-12 15:40:37 -0700

    Added synonym to random field

--- cbeImageDB/images/documents.py
+++ cbeImageDB/images/documents.py
@@ -1,11 +1,55 @@
 # code from github example
 from django_elasticsearch_dsl import Document
+from django_elasticsearch_dsl.documents import model_field_class_to_field_class
+from django_elasticsearch_dsl.exceptions import ModelFieldNotMappedError
+from elasticsearch_dsl import analyzer, token_filter, DocType, Text
+from django.conf import settings
 from django_elasticsearch_dsl.registries import registry
 from .models import Image, Experiment
+
+#index = Index(settings.ES_INDEX)
+#index.settings(**settings.ES_INDEX_SETTINGS)
+
+
+synonym_tokenfilter = token_filter(
+    'synonym_tokenfilter',
+    'synonym',
+    synonyms=[
+        'reactjs, react',  # <-- important
+    ],
+)
+
+text_analyzer = analyzer(
+    'text_analyzer',
+    tokenizer='standard',
+    filter=[
+        # The ORDER is important here.
+        'standard',
+        'lowercase',
+        'stop',
+        synonym_tokenfilter,
+        # Note! 'snowball' comes after 'synonym_tokenfilter'
+        'snowball',
+    ],
+    char_filter=['html_strip']
+)
+
+
+html_strip = analyzer(
+    'html_strip',
+    tokenizer="standard",
+    filter=["lowercase", "stop", "snowball"],
+        char_filter=["html_strip"]
+)
 
 
 @registry.register_document
 class ExperimentDocument(Document):
+    ugh = Text(
+        required=True, 
+        analyzer=text_analyzer
+    )
+    
     class Index:
         # Name of the Elasticsearch index
         name = 'experiment_names'
@@ -21,6 +65,8 @@ class ExperimentDocument(Document):
             'name',
         ]
 
+
+   
 
 @registry.register_document
 class ImageDocument(Document):
@@ -47,3 +93,52 @@ class ImageDocument(Document):
         # Paginate the django queryset used to populate the index with the specified size
         # (by default there is no pagination)
         # queryset_pagination = 5000
+
+
+
+from elasticsearch_dsl import (
+    DocType,
+    Text,
+    Index,
+    analyzer,
+    Keyword,
+    token_filter,
+)
+from django.conf import settings
+
+
+#index = Index(settings.ES_INDEX)
+#index.settings(**settings.ES_INDEX_SETTINGS)
+
+
+synonym_tokenfilter = token_filter(
+    'synonym_tokenfilter',
+    'synonym',
+    synonyms=[
+        'reactjs, react',  # <-- important
+    ],
+)
+
+text_analyzer = analyzer(
+    'text_analyzer',
+    tokenizer='standard',
+    filter=[
+        # The ORDER is important here.
+        'standard',
+        'lowercase',
+        'stop',
+        synonym_tokenfilter,
+        # Note! 'snowball' comes after 'synonym_tokenfilter'
+        'snowball',
+    ],
+    char_filter=['html_strip']
+)
+
+class BlogItemDoc(DocType):
+    title = Text(
+        required=True, 
+        analyzer=text_analyzer
+    )
+    text = Text(analyzer=text_analyzer)
+
+#index.doc_type(BlogItemDoc)
