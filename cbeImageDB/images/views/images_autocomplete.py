e1400414f7297721def3bc5bd362fa69a1394d22
EarthSquirrel <britneyandcaddy1@gmail.com>
2020-01-13 13:13:22 -0700

    Added comments

--- cbeImageDB/images/views/images_autocomplete.py
+++ cbeImageDB/images/views/images_autocomplete.py
@@ -1,13 +1,18 @@
+'''All of the autocomplete views are stored here accept for the search
+autocomplete view, because it is more involved. Some autocompletes have
+the capability to add new models others do not.'''
 from images import models
 from dal import autocomplete
 from images import search_utils as su
 
 
 # ######################### Autocomplete classes #############################
-# SearchAutocopmlete is in search.py
+# SearchAutocopmlete is in images_search.py
 class MicroscopeSettingAutocomplete(autocomplete.Select2QuerySetView):
-
+    '''Will bring up the microscope setting when searching with associated
+    scopes, mediums, and objectives.'''
     def get_queryset(self):
+        # List of all things possible to filter by
         settings = models.MicroscopeSettings.objects.all()
         scopes = models.Microscope.objects.all()
         mediums = models.ObjectiveMedium.objects.all()
@@ -32,6 +37,7 @@ class MicroscopeSettingAutocomplete(autocomplete.Select2QuerySetView):
 
 
 class MicroscopeAutocomplete(autocomplete.Select2QuerySetView):
+    '''Autocomplete: return a list of all the microscopes'''
 
     def get_queryset(self):
         qs = models.Microscope.objects.all()
@@ -43,7 +49,7 @@ class MicroscopeAutocomplete(autocomplete.Select2QuerySetView):
 
 
 class ObjectiveMediumAutocomplete(autocomplete.Select2QuerySetView):
-
+    '''Autocomplete: Returns a list of all the microscope objective mediums'''
     def get_queryset(self):
         qs = models.ObjectiveMedium.objects.all()
         if self.q:
@@ -51,17 +57,8 @@ class ObjectiveMediumAutocomplete(autocomplete.Select2QuerySetView):
         return qs
 
 
-class ImagerAutocomplete(autocomplete.Select2QuerySetView):
-
-    def get_queryset(self):
-        qs = models.Imager.objects.all()
-        if self.q:
-            qs = qs.filter(name__icontains=self.q)
-        return qs
-
-
 class OrganismAutocomplete(autocomplete.Select2QuerySetView):
-
+    '''Autocomplete: Return a list of all organisms'''
     def get_queryset(self):
         qs = models.Organism.objects.all()
         if self.q:
@@ -69,7 +66,17 @@ class OrganismAutocomplete(autocomplete.Select2QuerySetView):
         return qs
 
 
+class ImagerAutocomplete(autocomplete.Select2QuerySetView):
+    '''Autocomplete: Return a list of all imagers'''
+    def get_queryset(self):
+        qs = models.Imager.objects.all()
+        if self.q:
+            qs = qs.filter(name__icontains=self.q)
+        return qs
+
+
 class AddImagerAutocomplete(autocomplete.Select2QuerySetView):
+    '''Autocomplete: Can add an imager if not in the list of all imagers'''
     create_field = 'name'
     model = models.Imager
     model_field_name = 'name'
@@ -78,15 +85,19 @@ class AddImagerAutocomplete(autocomplete.Select2QuerySetView):
     def has_add_permission(self, request):
         return True
 
-    """
+
+class ProjectAutocomplete(autocomplete.Select2QuerySetView):
+    '''Autocomplete: Return a list of all projects'''
     def get_queryset(self):
-        qs = models.Imager.objects.all()
+        qs = models.Project.objects.all()
         if self.q:
             qs = qs.filter(name__icontains=self.q)
         return qs
-    """
+
 
 class AddProjectAutocomplete(autocomplete.Select2QuerySetView):
+    '''Autocomplete: Add a project if doesn't exist in the list of all 
+    projects returned'''
     create_field = 'name'
     model = models.Project
     model_field_name = 'name'
@@ -97,7 +108,7 @@ class AddProjectAutocomplete(autocomplete.Select2QuerySetView):
 
 
 class LabAutocomplete(autocomplete.Select2QuerySetView):
-
+    '''Autocomplete: Return a list of all labs'''
     def get_queryset(self):
         qs = models.Lab.objects.all()
         if self.q:
@@ -106,7 +117,7 @@ class LabAutocomplete(autocomplete.Select2QuerySetView):
 
 
 class GrowthSubstratumAutocomplete(autocomplete.Select2QuerySetView):
-
+    '''Autocomplete: Return a list of all growth substratum'''
     def get_queryset(self):
         qs = models.GrowthSubstratum.objects.all()
         if self.q:
@@ -115,7 +126,7 @@ class GrowthSubstratumAutocomplete(autocomplete.Select2QuerySetView):
 
 
 class VesselAutocomplete(autocomplete.Select2QuerySetView):
-
+    '''Autocomplete: Return list of vessels sample was grown in'''
     def get_queryset(self):
         qs = models.Vessel.objects.all()
         if self.q:
@@ -123,23 +134,14 @@ class VesselAutocomplete(autocomplete.Select2QuerySetView):
         return qs
 
 
-class ProjectAutocomplete(autocomplete.Select2QuerySetView):
-
-    def get_queryset(self):
-        qs = models.Project.objects.all()
-        if self.q:
-            qs = qs.filter(name__icontains=self.q)
-        return qs
-
-
 class DayAutocomplete(autocomplete.Select2ListView):
-
+    '''Autocomplete: Returns the numbers 1-31 for days of the month'''
     def get_list(self):
         return range(1, 32)
 
 
 class MonthAutocomplete(autocomplete.Select2ListView):
-
+    '''Autocomplete: Returns list of all possible months'''
     def get_list(self):
         months = ['January', 'Febuary', 'March', 'April', 'May', 'June',
                   'July', 'August', 'September', 'October', 'November',
@@ -148,6 +150,6 @@ class MonthAutocomplete(autocomplete.Select2ListView):
 
 
 class YearAutocomplete(autocomplete.Select2ListView):
-
+    '''Autocomplete: Returns a list of years that exist in the database'''
     def get_list(self):
         return su.get_year_list()
