f5b150f2d9303a001ac65e0dcbc6262a8552b2e6
EarthSquirrel <britneyandcaddy1@gmail.com>
2020-02-18 11:16:01 -0700

    Added 0.0.0.0 to allowed hosts

--- cbeImageDB/cbeImageDB/settings.py
+++ cbeImageDB/cbeImageDB/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '7ssu$(qg8oo=1_ul68^e&#wucef*&z9-!+^&uvc@um_)3&h&^l'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0']
 
 
 # Application definition
