bce0f01d97ae0fef29f1d8238805d499928fa6da
EarthSquirrel <britneyandcaddy1@gmail.com>
2020-02-26 09:37:53 -0700

    Added static root for collectstatic

--- cbeImageDB/cbeImageDB/settings.py
+++ cbeImageDB/cbeImageDB/settings.py
@@ -144,6 +144,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = config('STATIC_ROOT')
 
 # Path for storing files
 MEDIA_ROOT = config('MEDIA_ROOT')
--- cbeImageDB/cbeImageDB/urls.py
+++ cbeImageDB/cbeImageDB/urls.py
@@ -23,4 +23,4 @@ urlpatterns = [
     path('', views.AboutSite.as_view(), name='about'),
     path('images/', include('images.urls')),
     path('admin/', admin.site.urls),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
