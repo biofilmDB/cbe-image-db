a835bcea84ae92df07b2b6938e5bb47aa7c00be4
EarthSquirrel <britneyandcaddy1@gmail.com>
2020-02-27 11:33:02 -0700

    Migrate and initialize database

--- cbeImageDB/init.sh
+++ cbeImageDB/init.sh
@@ -15,10 +15,19 @@ python $manage collectstatic
 
 # Download ncbi taxonomy from PURL link on page:
 # http://www.obofoundry.org/ontology/ncbitaxon.html
-# TODO: If this fails, abort rest of script and give error message
 echo -e "\n***** Downloading NCBI taxonomy *****"
 curl -L http://purl.obolibrary.org/obo/ncbitaxon.owl > organisms/ncbitaxon.owl
 
 # Make organism file
 echo -e "\n***** Creating organism files *****"
 python organisms/parse_ncbi.py organisms/ncbitaxon.owl
+rm organisms/bad-organisms.csv
+
+# Make the migrations
+echo -e "\n***** Migrating *****"
+python $manage migrate
+
+# Initialize the database
+echo -e "\n***** Initialize the database *****"
+echo "      Assuming files already copied to docker "
+python $manage initdb
