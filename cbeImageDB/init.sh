1e9c66756d1aa7aa01bdd70955b64d2a0048cc2f
EarthSquirrel <britneyandcaddy1@gmail.com>
2020-03-06 09:45:30 -0700

    Added back create organism file commands

--- cbeImageDB/init.sh
+++ cbeImageDB/init.sh
@@ -13,6 +13,16 @@ manage=manage.py
 echo "***** Collecting static files in STATIC_ROOT *****"
 python $manage collectstatic
 
+# Download ncbi taxonomy from PURL link on page:
+# http://www.obofoundry.org/ontology/ncbitaxon.html
+echo -e "\n***** Downloading NCBI taxonomy *****"
+curl -L http://purl.obolibrary.org/obo/ncbitaxon.owl > organisms/ncbitaxon.owl
+
+# Make organism file
+echo -e "\n***** Creating organism files *****"
+python organisms/parse_ncbi.py organisms/ncbitaxon.owl
+rm organisms/bad-organisms.csv
+
 # Make the migrations
 echo -e "\n***** Migrating *****"
 python $manage migrate
