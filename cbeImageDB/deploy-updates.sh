7a1d9bd5516fe1aa8de0ac32bed74c50f3f916a5
EarthSquirrel <britneyandcaddy1@gmail.com>
2020-09-12 12:23:09 -0600

    Uncommented out git pull

--- cbeImageDB/deploy-updates.sh
+++ cbeImageDB/deploy-updates.sh
@@ -1,5 +1,5 @@
-# Should this be here since you have to enter cradentials?
-# git pull
+# CBE machine will have a read only access ssh key
+git pull
 
 # rebuild the container with new updates
 docker-compose -f ../docker-compose.yml -f ../docker-compose-web.yml build
