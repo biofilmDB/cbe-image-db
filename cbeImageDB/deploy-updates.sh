7b7128486a7fe434c5e304a62e965f449f2f3bc7
EarthSquirrel <britneyandcaddy1@gmail.com>
2020-09-14 07:50:18 -0600

    Added new line

--- cbeImageDB/deploy-updates.sh
+++ cbeImageDB/deploy-updates.sh
@@ -1,7 +1,11 @@
 # CBE machine will have a read only access ssh key
+echo -e "\033[1;34mUpdating local from GitHub\033[0m"
 git pull
 
 # rebuild the container with new updates
+echo -e "\n\033[1;34mRebuilding docker containers\033[0m"
 docker-compose -f ../docker-compose.yml -f ../docker-compose-web.yml build
+echo -e "\n\033[1;34mStopping all docker containers\033[0m"
 docker-compose stop
+echo -e "\n\033[1;34mRestarting docker\033[0m"
 docker-compose -f ../docker-compose.yml -f ../docker-compose-web.yml up
