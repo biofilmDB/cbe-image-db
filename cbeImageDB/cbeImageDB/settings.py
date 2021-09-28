147fc3989e26d07f1c0616f87ef35b0a11180354
EarthSquirrel <britneyandcaddy1@gmail.com>
2021-09-28 17:05:24 -0600

    Added S3Connection thing

--- cbeImageDB/cbeImageDB/settings.py
+++ cbeImageDB/cbeImageDB/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 from decouple import config
+from boto.s3.connection import S3Connection
+
+s3 = S3Connection(os.enviorn['DJANGO_SECRET_KEY'])
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
