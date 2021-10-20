30c655491cb5e5f28507c322be81689da2969f96
EarthSquirrel <britneyandcaddy1@gmail.com>
2021-10-20 15:43:34 -0600

    Added stuff to echo

--- cbeImageDB/startup.sh
+++ cbeImageDB/startup.sh
@@ -2,8 +2,8 @@ export PASSANGER_PORT=$PORT
 python manage.py collectstatic
 python manage.py makemigrations
 python manage.py migrate
-echo $PORT
-echo $PASSENGER_PORT
+echo "port: $PORT"
+echo "passanger port: $PASSENGER_PORT"
 # commenting out until elatci search works
 #python manage.py search_index --create
 passenger start
