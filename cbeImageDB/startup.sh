0ddb21938cbf045b415a63358c81b429a07caf02
EarthSquirrel <britneyandcaddy1@gmail.com>
2021-10-20 11:26:47 -0600

    Added comment...

--- cbeImageDB/startup.sh
+++ cbeImageDB/startup.sh
@@ -1,5 +1,6 @@
 python manage.py collectstatic
 python manage.py makemigrations
 python manage.py migrate
+# commenting out until elatci search works
 #python manage.py search_index --create
 passenger start
