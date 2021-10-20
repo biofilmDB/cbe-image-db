6ab4571f170930de1c5e7165291cb9340d75444e
EarthSquirrel <britneyandcaddy1@gmail.com>
2021-10-20 11:17:29 -0600

    Run startup.sh and remove collect static

--- cbeImageDB/startup.sh
+++ cbeImageDB/startup.sh
@@ -1,4 +1,5 @@
+python manage.py collectstatic
 python manage.py makemigrations
 python manage.py migrate
-python manage.py search_index --create
+#python manage.py search_index --create
 passenger start
