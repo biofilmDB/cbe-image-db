c13d868ab361e992db523d7707d821df7e2e5bae
EarthSquirrel <britneyandcaddy1@gmail.com>
2021-10-27 11:06:52 -0600

    Added populate testing

--- cbeImageDB/startup.sh
+++ cbeImageDB/startup.sh
@@ -2,6 +2,7 @@ export PASSANGER_PORT=$PORT
 python manage.py collectstatic
 python manage.py makemigrations
 python manage.py migrate
+python manage.py populatetesting
 echo "port: $PORT"
 echo "passanger port: $PASSENGER_PORT"
 # commenting out until elatci search works
