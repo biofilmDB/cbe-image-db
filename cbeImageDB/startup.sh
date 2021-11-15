5b8eb71ed6aeb0da5a0782b144c4d4f30a52f0c6
EarthSquirrel <britneyandcaddy1@gmail.com>
2021-11-15 15:08:55 -0700

    populate the elastic search db

--- cbeImageDB/startup.sh
+++ cbeImageDB/startup.sh
@@ -2,6 +2,7 @@ python manage.py migrate
 python manage.py populatetesting
 # build the elasticsearch index
 python manage.py search_index --create
+python manage.py search_index --populate
 # run passenger on the specified port
 # $PORT is a changing port number on heroku
 passenger start --port $PORT
